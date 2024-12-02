02ae601d8fac0b08412c229f316199fc4446f1da
Your Name <youremail@domain.com>
2024-12-03 02:46:12 +0300

    update on the inheritance of the vehicle to car

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,9 @@
 class Vehicle:
+    def __init__(self,wheel_size, wheel_number) -> None:
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+    def go(self) -> None:
+        return "vrrrrrrrooom!"
+    def fill_up_tank(self) -> None:
+        return "filling up!"
     pass
